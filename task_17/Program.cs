03aa18b96563bcb973f0e8315636f9023a7a6bce
Эдуард Шамсиев <s-6001@yandex.ru>
2021-12-16 12:54:57 +0500

    Добавьте файлы проекта.

---
+++ task_17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+
+    class Bill<T>
+    {
+        public T BillNumber { get; set; }
+        public double Balance { get; set; }
+        public string Name { get; set; }
+    }
+}
