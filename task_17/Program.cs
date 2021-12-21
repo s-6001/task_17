0f3fced26b6c7672cb414be9951a716bcf2defd0
Эдуард Шамсиев <s-6001@yandex.ru>
2021-12-21 12:38:51 +0500

    доведение до рабочего состояния

--- task_17/Program.cs
+++ task_17/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace task_17
 {
@@ -10,12 +6,45 @@ namespace task_17
     {
         static void Main(string[] args)
         {
-            Bill<int> bill1 = new Bill<int>(1,1,"1");
-            Console.WriteLine("Введите номер счета");
-            
+            Bill<string> bill1 = new Bill<string>("0", 0, "0"); //с номером счета string
+            Console.WriteLine("Введите номер счета string");
+            bill1.BillNumber = Console.ReadLine();  //считываем номер счета string
+            Console.WriteLine("Введите баланс");
+            try
+            {
+                bill1.Balance = Convert.ToInt32(Console.ReadLine());    //считываем баланс
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+            Console.WriteLine("Введите ФИО");
+            bill1.Name = Console.ReadLine();    //считываем ФИО
+            Console.WriteLine();
+            Bill<int> bill2 = new Bill<int>(0, 0, "0"); //с номером счета int
+            Console.WriteLine("Введите номер счета int");
+            try
+            {
+                bill2.BillNumber = Convert.ToInt32(Console.ReadLine()); //считываем номер счета int
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+            Console.WriteLine("Введите баланс");
+            try
+            {
+                bill2.Balance = Convert.ToInt32(Console.ReadLine());    //считываем баланс
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка ввода");
+            }
+            Console.WriteLine("Введите ФИО");
+            bill2.Name = Console.ReadLine();    //считываем ФИО
+            Console.ReadKey();
         }
     }
-
     class Bill<T>
     {
         public T BillNumber { get; set; }
@@ -23,9 +52,9 @@ namespace task_17
         public string Name { get; set; }
         public Bill(T bn, double bal, string n)
         {
-            BillNumber = bn;
-            Balance = bal;
-            Name = n;
+            BillNumber = bn;    //номер счета
+            Balance = bal;  //баланс
+            Name = n;   //ФИО
         }
     }
 }
